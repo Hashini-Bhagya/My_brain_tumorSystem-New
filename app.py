3b4fae871b2f8fc4b72b8e9e98461847bac7e26f
Hashini-Bhagya <kavindihashini15@gmail.com>
2025-08-22 20:45:54 +0530

    login_done

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-from flask import Flask, request, render_template, send_from_directory
-import os
+from flask import Flask
+from config import Config
+from src.utils.db import db, test_database_connection
+from src.routes.api import api_bp
+from src.routes.main_routes import main_bp
+from flask_login import LoginManager
+from src.utils._init_ import create_app
 
-from src.routes.api import api_blueprint
-
-
-app = Flask(__name__)
-
-# Register the blueprint
-app.register_blueprint(api_blueprint)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app = create_app()
+    if app:
+        app.run(debug=True, host="0.0.0.0", port=5000)
